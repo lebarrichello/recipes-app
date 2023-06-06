faa5136a6bff05cf6bb2bcfaafc912c76eaf6383
Darlan Leandro <darlanleandro82@gmail.com>
2023-06-06 16:11:02 -0300

    requisito 19, renderizando comidas e bebidas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter, Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Components/Login';
-import Meals from './Paginas/Meals';
 import Profile from './Paginas/Profile';
-import Drinks from './Paginas/Drinks';
+import Recipes from './Paginas/Recipes';
 import Done from './Paginas/DoneRecipes';
 import Favorites from './Paginas/FavoriteRecipes';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route path="/meals" component={ Meals } />
-      <Route path="/drinks" component={ Drinks } />
-      <Route path="/profile" component={ Profile } />
-      <Route path="/done-recipes" component={ Done } />
-      <Route path="/favorite-recipes" component={ Favorites } />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/meals" component={ Recipes } />
+        <Route path="/drinks" component={ Recipes } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ Done } />
+        <Route path="/favorite-recipes" component={ Favorites } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/Paginas/Recipes.js
+++ src/Paginas/Recipes.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Meals from './Meals';
+import Drinks from './Drinks';
 
 function Recipe() {
+  const [recipes, setRecipes] = useState([]);
+  const path = window.location.pathname;
+  const foodPath = path === '/meals';
+  const API_FOOD = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+  const API_DRINK = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+  const NUMBER = 11;
+
+  useEffect(() => {
+    if (foodPath) {
+      fetch(API_FOOD)
+        .then((response) => response.json())
+        .then(({ food }) => {
+          const recipesData = food.slice(0, NUMBER);
+          setRecipes(recipesData);
+        });
+    } else {
+      fetch(API_DRINK)
+        .then((response) => response.json())
+        .then(({ drinks }) => {
+          const recipesData = drinks.slice(0, NUMBER);
+          setRecipes(recipesData);
+        });
+    }
+  }, [foodPath]);
   return (
-    <div>Recipe</div>
+    <main>
+      { foodPath ? recipes.map((recipe, i) => (
+        <Meals
+          key={ recipe.strMeal }
+          name={ recipe.strMeal }
+          imgSrc={ recipe.strMealThumb }
+          SrcIndex={ i }
+        />
+      )) : recipes.map((recipe, i) => (
+        <Drinks
+          key={ recipe.strDrink }
+          name={ recipe.strDrink }
+          imgSrc={ recipe.strDrinkThumb }
+          SrcIndex={ i }
+        />
+      ))}
+    </main>
   );
 }
 export default Recipe;
